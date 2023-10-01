1a4b9fc0a1596dee8b0801970b98bbf3f2fe6e10
Pedro Júnior <pedrojuniorifpb@gmail.com>
2023-10-01 12:21:59 -0300

    adicionando prisma no projeto

---
+++ prisma/migrations/20231001151547_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notepads" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "notepads_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_id_idx" ON "users"("id");
+
+-- CreateIndex
+CREATE INDEX "notepads_id_idx" ON "notepads"("id");
+
+-- CreateIndex
+CREATE INDEX "notepads_userId_idx" ON "notepads"("userId");
+
+-- AddForeignKey
+ALTER TABLE "notepads" ADD CONSTRAINT "notepads_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
